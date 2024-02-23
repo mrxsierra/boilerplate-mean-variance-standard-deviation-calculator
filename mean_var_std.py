c9cba805bde381431b7a94b09ba4b8b9b8c4b3b9
Sunil Sharma <9.sunilsharma@gmail.com>
2024-02-23 09:31:34 +0000

    created a function for statstical calculation

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,50 @@
 import numpy as np
 
-def calculate(list):
+def calculate(l):
+    """
+    Perform calculations on a list of nine numbers.
 
+    Parameters:
+    - l (list): A list containing nine numeric values.
 
+    Returns:
+    dict: A dictionary containing various statistical calculations based on the input list.
+    
+    The calculations are based on rows, columns, and flattened array.
 
+    Raises:
+    ValueError: If the length of the input list is not equal to 9.
+
+    Examples:
+    >>> calculate([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    {
+        'mean': [[4.0, 5.0, 6.0], [2.0, 5.0, 8.0], 5.0],
+        'variance': [[6.0, 6.0, 6.0], [0.6666666666666666, 0.6666666666666666, 0.6666666666666666], 6.666666666666667],
+        'standard deviation': [[2.449489742783178, 2.449489742783178, 2.449489742783178], [0.816496580927726, 0.816496580927726, 0.816496580927726], 2.581988897471611],
+        'max': [[7, 8, 9], [3, 6, 9], 9],
+        'min': [[1, 2, 3], [1, 4, 7], 1],
+        'sum': [[12, 15, 18], [6, 15, 24], 45]
+    }
+
+    """
+    if len(l) == 9:
+        arr = np.array(l).reshape(3, 3)
+        mean = [list(arr.mean(axis=0)), list(arr.mean(axis=1)), arr.mean()]
+        variance = [list(arr.var(axis=0)), list(arr.var(axis=1)), arr.var()]
+        std = [list(arr.std(axis=0)), list(arr.std(axis=1)), arr.std()]
+        max_val = [list(arr.max(axis=0)), list(arr.max(axis=1)), arr.max()]
+        min_val = [list(arr.min(axis=0)), list(arr.min(axis=1)), arr.min()]
+        sum_val = [list(arr.sum(axis=0)), list(arr.sum(axis=1)), arr.sum()]
+
+        calculations = {
+            "mean": mean,
+            "variance": variance,
+            "standard deviation": std,
+            "max": max_val,
+            "min": min_val,
+            "sum": sum_val,
+        }
+    else:
+        raise ValueError("List must contain nine numbers.")
 
     return calculations
